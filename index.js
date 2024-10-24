f62bfa669c6820c48c87b1b339dc5b7b281014cb
Olakareem <olakareemomobolarinwa@gmail.com>
2024-10-24 22:22:37 +0100

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import express from "express";
+// import morgan from "morgan";
+// import { errorHandler, routeNotFound } from "./middlewares/errorMiddlewaves.js";
+// import routes from "./routes/index.js";
+// import { dbConnection } from "./utils/index.js";
+
+// dotenv.config();
+
+// dbConnection();
+
+// const PORT = process.env.PORT || 5000;
+
+// const app = express();
+
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000", "http://localhost:3001"],
+//     methods: ["GET", "POST", "DELETE", "PUT"],
+//     credentials: true,
+//   })
+// );
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(cookieParser());
+
+// app.use(morgan("dev"));
+// app.use("/api", routes);
+
+// app.use(routeNotFound);
+// app.use(errorHandler);
+
+// app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -10,8 +47,6 @@ import { dbConnection } from "./utils/index.js";
 dotenv.config();
 
 dbConnection();
-
-const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -34,4 +69,5 @@ app.use("/api", routes);
 app.use(routeNotFound);
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+// Instead of app.listen(), export the app
+export default app;
