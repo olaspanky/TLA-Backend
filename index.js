07c95bb1968b036e70a23e39ce8512a3b4335d93
olaspanky <olakareemomobolarinwa@gmail.com>
2024-10-25 07:42:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://pbr-tla.netlify.app"],
+    origin: ["http://localhost:3000", "https://pbr-tla.netlify.app", "https://tla-two.vercel.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
