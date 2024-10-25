c5680e002dc4e05e76ea7a7cd82e6df0e0e69e02
olaspanky <olakareemomobolarinwa@gmail.com>
2024-10-25 08:03:10 +0100

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -19,8 +19,8 @@ export const createJWT = (res, userId) => {
   // Change sameSite from strict to none when you deploy your app
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict", //prevent CSRF attack
-    maxAge: 1 * 24 * 60 * 60 * 1000, //1 day
+    secure: isProduction, // Secure in production
+    sameSite: isProduction ? "None" : "Lax", // None for production, Lax for dev
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
   });
 };
